95906742cf551caca3c1542ffc2132d480e59775
metacious <miraunmax@hotmail.com>
2023-11-08 02:28:28 -0400

    Modelo familiar agregado

--- familia/models.py
+++ familia/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Familiar(models.Model):
+    nombre = models.CharField(max_length=256)
+    apellido = models.CharField(max_length=256)
+    parentezco_conmigo = models.CharField(max_length=256)
+    fecha_nacimiento = models.DateField(null=True, blank=True)
+    rut = models.IntegerField(max_length=16, null=True, blank=True)
+
